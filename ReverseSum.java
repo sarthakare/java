93207aed76051cea681ad479347ce94b7324fe77
sarthakare <85685165+sarthakare@users.noreply.github.com>
2022-03-10 16:14:27 +0530

    Add files via upload

---
+++ ReverseSum.java
@@ -0,0 +1,21 @@
+import  java.util.*;
+class ReverseSum
+{
+public static void main(String args[])
+{
+int num,rem, sum = 0;
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter the number : ");
+num = sc.nextInt();
+System.out.println("Reverse order is : ");
+while(num > 0)
+{
+rem = num%10;
+sum = rem + sum;
+System.out.print(rem);
+num = num / 10;
+}
+System.out.println("\n Sum of digit in entered no : " + sum);
+}
+}
+
